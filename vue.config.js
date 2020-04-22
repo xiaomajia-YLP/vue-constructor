66bd9a3169ef1802b684b1bfb4c7021d2803e7d6
xiaomajia-YLP <1134126206@qq.com>
2020-04-22 09:36:57 +0800

    project init

---
+++ vue.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const resolve = (dir) => {
+  return path.join(__dirname, dir);
+};
+
+const isPro = process.env.NODE_ENV === "production";
+
+module.exports = {
+  assetsDir: "static",
+  publicPath: isPro ? "xiaomajia/" : "/",
+  indexPath: "index.html",
+  filenameHashing: true,
+  lintOnSave: false,
+  runtimeCompiler: false,
+  productionSourceMap: false,
+  configureWebpack: {
+    externals: {},
+  },
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set("@", resolve("src"))
+      .set("@lib", resolve("src/libs"))
+      .set("@com", resolve("src/components"))
+      .set("@view", resolve("src/views"))
+      .set("@img", resolve("src/assets/images"));
+    config.module
+      .rule("images")
+      .use("url-loader")
+      .loader("url-loader")
+      .tap((options) => Object.assign(options, { limit: 10240 }));
+  },
+  css: {
+    modules: false,
+    // extract: isPro,
+    sourceMap: false,
+  },
+  devServer: {
+    proxy: {
+      "/admin": {
+        target: "http://xx.xx.xx.xx:8080/", // 代理的接口路径
+        // ws: true,   // 是否开启webscoket
+        changeOrigin: true,
+        secure: false, // 是否启用https
+        pathRewrite: {
+          "^/admin": "",
+        },
+      },
+    },
+  },
+  pluginOptions: {
+    // 用来传递任何第三方插件选项。Type: Object
+  },
+};
